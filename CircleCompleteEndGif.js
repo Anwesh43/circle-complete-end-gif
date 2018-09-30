33837c33b2de0ec2700812c25848a8f459aea3df
Anwesh43 <anweshthecool0@gmail.com>
2018-09-30 14:05:52 +0530

    adding State class

---
+++ CircleCompleteEndGif.js
@@ -0,0 +1,25 @@
+const w = 500, h = 500, nodes = 5
+
+class State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += 0.025 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating() {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+        }
+    }
+}
